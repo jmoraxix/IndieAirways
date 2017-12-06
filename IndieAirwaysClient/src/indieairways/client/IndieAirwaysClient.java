754a0e7a738d90d75ec07c02dac0cb7426b48cf2
Jose Mora <jdevmora@gmail.com>
2017-12-05 23:13:09 -0600

    Created new client menu window and JavaFX main class.

---
+++ IndieAirwaysClient/src/indieairways/client/IndieAirwaysClient.java
@@ -0,0 +1,111 @@
+/*
+* Programacion Concurrente Cliente Servidor
+* 
+* Emilio Evans Rodriguez
+* Jose David Mora Loria
+* Carlos Oreamuno Alfaro
+* 
+* Tercer cuatrimestre, 2017
+* Ulacit
+ */
+package indieairways.client;
+
+import indieairways.util.Util;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.fxml.JavaFXBuilderFactory;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author jmora
+ */
+public class IndieAirwaysClient extends Application {
+
+    private Stage stage;
+    private final double WINDOW_WIDTH = Util.WIDTH;
+    private final double WINDOW_HEIGHT = Util.HEIGHT;
+
+    //Variables del cliente TCP
+//    private Cliente cliente;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Application.launch(IndieAirwaysClient.class, (java.lang.String[]) null);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            stage = primaryStage;
+            stage.setTitle("Cine");
+            stage.setMinWidth(WINDOW_WIDTH);
+            stage.setMinHeight(WINDOW_HEIGHT);
+            gotoMenu();
+            primaryStage.show();
+        } catch (Exception ex) {
+            Logger.getLogger(IndieAirwaysClient.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        //Iniciando cliente TCP
+//        this.cliente = new Cliente(this);
+//        this.cliente.start();
+    }
+
+    /**
+     * Cambia el contenido de la ventana al menu principal
+     */
+    public void gotoMenu() {
+        try {
+            MenuClienteController menu = (MenuClienteController) replaceSceneContent("MenuCliente.fxml");
+            menu.setApp(this);
+        } catch (Exception ex) {
+            Logger.getLogger(IndieAirwaysClient.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Cambia el contenido de la ventana a la venta de las peliculas
+     */
+    public void gotoVerPeliculas() {
+//        try {
+//            VerPeliculasController verPeliculas = (VerPeliculasController) replaceSceneContent("VerPeliculas.fxml");
+//            verPeliculas.setApp(this);
+//        } catch (Exception ex) {
+//            Logger.getLogger(IndieAirwaysClient.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+    }
+
+    private Initializable replaceSceneContent(String fxml) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        InputStream in = IndieAirwaysClient.class.getResourceAsStream(fxml);
+        loader.setBuilderFactory(new JavaFXBuilderFactory());
+        loader.setLocation(IndieAirwaysClient.class.getResource(fxml));
+        AnchorPane page;
+        try {
+            page = (AnchorPane) loader.load(in);
+        } finally {
+            in.close();
+        }
+        Scene scene = new Scene(page, WINDOW_WIDTH, WINDOW_HEIGHT);
+        stage.setScene(scene);
+        stage.sizeToScene();
+        return (Initializable) loader.getController();
+    }
+
+    //Getters & Setters
+    /**
+     *
+     * @return Cliente Retorna la lista de clientes
+     */
+//    public Cliente getCliente() {
+//        return cliente;
+//    }
+}
---
+++ IndieAirwaysClient/src/indieairways/client/MenuClienteController.java
@@ -0,0 +1,60 @@
+/*
+ * Programacion Concurrente Cliente Servidor
+ * 
+ * Jose David Mora Loria
+ * Adrian Ruiz Porras
+ * Carlos Rodriguez Otoya
+ * 
+ */
+package indieairways.client;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * FXML Controller class
+ *
+ * @author jmora
+ */
+public class MenuClienteController implements Initializable {
+
+    @FXML
+    private ImageView btnPeliculas;
+    @FXML
+    private ImageView btnComida;
+    @FXML
+    private ImageView btnReservaciones;
+
+    private IndieAirwaysClient application;
+
+    /**
+     * Define la aplicacion principal para manejo de ventanas
+     *
+     * @param application
+     */
+    public void setApp(IndieAirwaysClient application) {
+        this.application = application;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        btnPeliculas.addEventHandler(MouseEvent.MOUSE_CLICKED, (MouseEvent e)
+                -> {
+            System.out.println("btnPeliculas");
+            application.gotoVerPeliculas();
+        });
+        btnComida.addEventHandler(MouseEvent.MOUSE_CLICKED, (MouseEvent e)
+                -> {
+            System.out.println("btnComida");
+        });
+        btnReservaciones.addEventHandler(MouseEvent.MOUSE_CLICKED, (MouseEvent e)
+                -> {
+            System.out.println("btnReservaciones");
+        });
+    }
+
+}
