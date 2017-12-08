2ddb81be7aabd661ce5f6602891c0c397111c0a5
Carlos Oreamuno <carlos.oreamunoalfaro@ucr.ac.cr>
2017-12-07 18:59:10 -0600

    With the Gson 2

--- IndieAirwaysCommon/src/indieairways/util/Util.java
+++ IndieAirwaysCommon/src/indieairways/util/Util.java
@@ -27,8 +27,8 @@ import javafx.collections.ObservableList;
 public class Util {
 
     //Variables de las ventanas
-    public static int WIDTH = 780;
-    public static int HEIGHT = 700;
+    public static int WIDTH = 642;
+    public static int HEIGHT = 528;
 
     public static int SERVER_PORT = 2356;
     public static String SERVER_IP = "localhost";
