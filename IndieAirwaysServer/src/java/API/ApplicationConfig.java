d42dbcf9d2f967c8e8e48bb489a49cbe4edf4d57
ulacit <ulacit@docente.ulacit.ac.cr>
2017-12-07 20:29:40 -0600

    Fixed server to create new RESTful web service from patterns

---
+++ IndieAirwaysServer/src/java/API/ApplicationConfig.java
@@ -0,0 +1,40 @@
+/*
+ * Programacion Concurrente Cliente Servidor
+ * 
+ * Emilio Evans Rodriguez
+ * Jose David Mora Loria
+ * Carlos Oreamuno Alfaro
+ * 
+ * Tercer cuatrimestre, 2017
+ * Ulacit
+ */
+package API;
+
+import java.util.Set;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author jmora
+ */
+@javax.ws.rs.ApplicationPath("webresources")
+public class ApplicationConfig extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new java.util.HashSet<>();
+        addRestResourceClasses(resources);
+        return resources;
+    }
+
+    /**
+     * Do not modify addRestResourceClasses() method.
+     * It is automatically populated with
+     * all resources defined in the project.
+     * If required, comment out calling this method in getClasses().
+     */
+    private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(API.UserResource.class);
+    }
+    
+}
