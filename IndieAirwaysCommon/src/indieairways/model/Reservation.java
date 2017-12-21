dd833d6ab8ef10c39391ac2e974ad4e8e4ae18f3
Jose Mora <jdevmora@gmail.com>
2017-12-21 03:48:13 -0600

    Updated reservation class from models.

--- IndieAirwaysCommon/src/indieairways/model/Reservation.java
+++ IndieAirwaysCommon/src/indieairways/model/Reservation.java
@@ -22,10 +22,10 @@ public class Reservation {
     private Flight flight;
     private int tickets, luggage;
     private String seats;
+
     /*
     @TODO Add sector to reservation
-    */
-
+     */
     public Reservation() {
     }
 
@@ -62,7 +62,7 @@ public class Reservation {
     }
 
     public int getTotalPrice() {
-        return (this.getFlight().getCost() * this.tickets) + 100 * (this.tickets-this.luggage<0?this.luggage-this.tickets:0);
+        return (this.getFlight().getCost() * this.tickets) + 100 * (this.tickets - this.luggage < 0 ? this.luggage - this.tickets : 0);
     }
 
     public String getSeats() {
@@ -71,6 +71,10 @@ public class Reservation {
 
     public void setSeats(String seats) {
         this.seats = seats;
+    }
+
+    public String getReservationCode() {
+        return reservationCode;
     }
 
     private String getNextCode() {
